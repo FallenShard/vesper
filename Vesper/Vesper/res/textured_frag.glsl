b1a9556dd35e7e4b543e722a83d00bca8d44ca08
FallenShard <nemanjabartolovic@yahoo.com>
2016-02-27 16:20:32 +0100

    Add gamma normalization in fragment shader

--- Vesper/Vesper/res/textured_frag.glsl
+++ Vesper/Vesper/res/textured_frag.glsl
@@ -9,7 +9,15 @@ out vec4 color;
 
 uniform sampler2D tex;
 
+float toSRGB(float value)
+{
+    if (value < 0.0031308)
+        return 12.92 * value;
+    return 1.055 * pow(value, 0.41666) - 0.055;
+}
+
 void main()
 {
-    color = texture(tex, vsOut.texCoord);
+    vec4 texel = texture(tex, vsOut.texCoord);
+    color = vec4(toSRGB(texel.r), toSRGB(texel.g), toSRGB(texel.b), 1.f);
 }
